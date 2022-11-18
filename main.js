b9c92cebad976cf59b3905faeb41aba393431c17
Supakorn Charoen <supakorncharoen@gmail.com>
2022-11-18 21:17:33 +0700

    Adding scrolling down hiding nav-bar

--- main.js
+++ main.js
@@ -16,9 +16,20 @@ const closeMenu = () => {
   navBar1.classList.remove("nav-active");
 };
 
+let prevScrollpos = window.pageYOffset;
 let navBar1 = document.querySelector(".nav-bar");
 let navBar = document.querySelector(".topnav");
+
 window.onscroll = function () {
+  // Create scrolling down hide nav-bar, and scrolling up show nav-bar
+  let currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    navBar1.style.top = "0";
+  } else {
+    navBar1.style.top = "-7rem";
+  }
+  prevScrollpos = currentScrollPos;
+
   // pageYOffset or scrollY
   if (
     window.pageYOffset > 0 ||
